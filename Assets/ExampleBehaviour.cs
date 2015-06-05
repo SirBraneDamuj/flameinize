57c4ab6cf543d186f497632f4b584732caa047f1
Zach Thacker <kon6236@ex.kroger.com>
2015-06-04 20:51:17 -0400

    Initial commit

---
+++ Assets/ExampleBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleBehaviour : MonoBehaviour {	
+	//this value can be changed in the inspector
+	public float interval = 3.0f;
+	public float jumpSpeed = 10.0f;
+	
+	float _timer;
+
+	// Use this for initialization
+	void Start () {
+		_timer = interval;
+	}
+	
+	// Update is called periodically (about once per frame)
+	void Update () {
+		_timer -= Time.deltaTime; //deltaTime = time elapsed since last Update
+		if(_timer <= 0) {
+			Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+			Vector2 velocity = rigidbody2D.velocity;
+			velocity.y = jumpSpeed;
+			rigidbody2D.velocity = velocity;
+			_timer = interval;
+		}
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float velocity = 1.0f; //how fast does Trogdor walk?	
+	//how much of his velocity does he lose per frame after you let go of the button?
+	public float deceleration = 0.1f;
+	public float jumpSpeed = 1.0f; //what is his initial jump velocity?
+	
+	bool canJump;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+		Vector2 currentVelocity = rigidbody2D.velocity;
+		float horizontal = Input.GetAxis("Horizontal");
+		if (horizontal != 0) {
+			currentVelocity.x = horizontal * velocity;
+		}
+		else {
+			float deceleratedX = currentVelocity.x * deceleration;
+			currentVelocity.x = deceleratedX <= 0.1f ? 0 : deceleratedX;
+		}
+		if (canJump && Input.GetKeyDown(KeyCode.Space)) {
+			canJump = false;
+			currentVelocity.y = jumpSpeed;
+		}
+		rigidbody2D.velocity = currentVelocity;
+	}
+	
+	void OnCollisionStay2D(Collision2D collision) {
+		if (collision.collider.tag == "Ground") {
+			canJump = true; //when Trogdor is on the ground, he's allowed to jump
+		}
+	}
+}
